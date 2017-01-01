97d4bc2fc1f9b8ae9260b9917b6de6c1608b8794
Richard <richard@RicharddeMacBook-Pro.local>
2017-01-01 16:28:54 +0800

    [MyComment] add a xcode source editor project.

---
+++ MyComment/MyComment/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyComment
+//
+//  Created by Richard on 2016/12/31.
+//  Copyright © 2016年 Richard. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
